0f4a73918f6c23a026ac9b869c91cbc66b152eb8
Vutrihau <vutrihau1@gmail.com>
2021-06-01 01:18:53 +0700

    update thêm gọi request cập nhật DB điểm danh

--- test/test2.py
+++ test/test2.py
@@ -5,19 +5,20 @@ import sys
 img = cv.imread(
     r"C:\Users\Huy Hoang PC\Desktop\github\Face_Recognize_InceptionResnetv1_TripletLoss\test\WIN_20210428_18_59_41_Pro.png")
 
-mtcnn_detector = MTCNN()
-# print(img.shape)
-# print(img.shape)
-# print(img[500])
-results = mtcnn_detector.detect_faces(img)
-print(results.data)
-x1, y1, width, height = results[0]['box']
-# print(x1, y1, width, height)
-img = img[y1:y1+height, x1:x1+width]
-
-if img is None:
-    sys.exit("Could not read the image.")
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+print(type(img))
+# mtcnn_detector = MTCNN()
+# # print(img.shape)
+# # print(img.shape)
+# # print(img[500])
+# results = mtcnn_detector.detect_faces(img)
+# # print(results.data)
+# x1, y1, width, height = results[0]['box']
+# # print(x1, y1, width, height)
+# img = img[y1:y1+height, x1:x1+width]
+# print(type(img))
+# if img is None:
+#     sys.exit("Could not read the image.")
+# cv.imshow("Display window", img)
+# k = cv.waitKey(0)
+# if k == ord("s"):
+#     cv.imwrite("starry_night.png", img)
--- testvideocapture.py
+++ testvideocapture.py
@@ -1,17 +1,17 @@
 import cv2
 cap = cv2.VideoCapture(0)
 
-while True:
+# while True:
 
-    # Read the frame
-    _, img = cap.read()
-    print(img)
-    # Stop if end of video file
-    if _ == False:
-        break
-    # Stop if escape key is pressed
-    key = cv2.waitKey(25) & 0xff
-    if key == 27:
-        break
+# Read the frame
+_, img = cap.read()
+print(img.data)
+# Stop if end of video file
+# if _ == False:
+#     break
+# # Stop if escape key is pressed
+# key = cv2.waitKey(25) & 0xff
+# if key == 27:
+#     break
 # Release the VideoCapture object
 cap.release()
